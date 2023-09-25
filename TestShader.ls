a601491971c0246dcf63bc8212f6a611b66aba0d
Daniel <144590047+000000000-1@users.noreply.github.com>
2023-09-25 15:30:33 +0200

    Added SRC

---
+++ TestShader.ls
@@ -0,0 +1,4 @@
+#Submit from Felix1337
+#Date 14.01.2023
+#Type Shadertoy
+#Main Render()
---
+++ i/dupx/launcher/CLAPI.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2020, HAZE. All rights reserved.
+ */
+package i.dupx.launcher;
+
+/**
+ * Mockup implementation for ClientLauncher API
+ *
+ * @author idupx
+ */
+public class CLAPI {
+    public static String getCLUsername() {
+        return "YourMockName";
+    }
+
+    public static String getCLToken() {
+        return "";
+    }
+
+    public static int getClientRole() {
+        return 10;
+    }
+
+    public static void sendIRCMessage(String msg) {
+    }
+}
